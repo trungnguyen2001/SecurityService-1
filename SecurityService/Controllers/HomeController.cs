2d4b393716a5a646aa6b51df90fe13c1853fdac2
Phats Lys <phatly2001@gmail.com>
2021-05-18 09:55:18 +0700

    UI Version 1.0

--- SecurityService/Controllers/HomeController.cs
+++ SecurityService/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SecurityService.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SecurityService.Controllers
 {
     public class HomeController : Controller
@@ -22,16 +16,7 @@ namespace SecurityService.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+       
     }
 }
--- SecurityService/wwwroot/js/site.js
+++ SecurityService/wwwroot/js/site.js
@@ -1,4 +1,98 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
 // Write your JavaScript code.
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    spaceBetween: 5,
+    effect: 'fade',
+    centeredSlides: true,
+    
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// search function
+
+$(document).ready(function () {
+    var myNav = document.getElementById("myNav");
+    var searchAround = document.getElementById("search-around");
+    $('#search').click(function () {
+        $('.myNav-item').addClass('hide-item');
+        $('.search-form').addClass('active');
+        $('.close').addClass('active');
+        $('#search').hide();
+        $('.search-dropdown').addClass('active');
+        myNav.style.backgroundColor = "rgba(0,0,0,1)";
+        searchAround.style.display = "block";
+        document.body.style.overflow = "hidden";
+        
+        
+        
+    })
+    $('.close').click(function () {
+        $('.myNav-item').removeClass('hide-item');
+        $('.search-form').removeClass('active');
+        $('.close').removeClass('active');
+        $('.search-dropdown').removeClass('active');
+        $('#search').show();
+        myNav.style.removeProperty("background-color");
+        searchAround.style.display = "none";
+        document.body.style.removeProperty("overflow");
+        
+    })
+})
+
+
+// navigation sticky
+
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myNav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+
+
+    } else {
+        header.classList.remove("sticky");
+
+    }
+};
+
+
+//hamberger menu
+
+$(document).ready(function () {
+    var myNav_list = document.getElementById('myNav-list');
+    var myNav = document.getElementById('myNav');
+    var navIcon = document.getElementById('nav-icon');
+    $('#menu-btn').click(function () {
+        if ($(this).prop("checked") == true) {
+            myNav_list.classList.add("active");
+            myNav.classList.add("active");
+            document.body.style.overflow = "hidden";
+            navIcon.classList.add("active");
+        } else {
+            myNav_list.classList.remove("active");
+            myNav.classList.remove("active");
+            document.body.style.removeProperty("overflow");
+            navIcon.classList.remove("active");
+
+        }
+    })
+})
